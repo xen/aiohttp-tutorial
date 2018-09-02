6e0b3a024ca0d87b2aca52191b08d8616fd3508e
Mikhail Kashkin <m@xen.ru>
2018-09-02 23:03:26 +0700

    На этом шаге мы:
    - Добавляем поддержку uvloop и ускоряем работу проекта
    - Обсуждаем конфигурирование
    - Добавляем опции командной строки для указания порта и хоста загрузки
    - Добавляем режим автоперезапуска при изменении кода

--- entry.py
+++ entry.py
@@ -1,7 +1,30 @@
+import argparse
+import asyncio
 import aiohttp
 from demo import create_app
 
+try:
+    import uvloop
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+except ImportError:
+    print("Library uvloop is not available")
+
+parser = argparse.ArgumentParser(description="Demo project")
+parser.add_argument('--host', help="Host to listen", default="0.0.0.0")
+parser.add_argument('--port', help="Port to accept connections", default=5000)
+parser.add_argument(
+    '--reload',
+    action="store_true",
+    help="Autoreload code on change")
+
+args = parser.parse_args()
+
 app = create_app()
 
+if args.reload:
+    print("Start with code reload")
+    import aioreloader
+    aioreloader.start()
+
 if __name__ == '__main__':
-    aiohttp.web.run_app(app, )
+    aiohttp.web.run_app(app, host=args.host, port=args.port)
