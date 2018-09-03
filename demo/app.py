6aa58ec95c9ff790f84417511deb228144c3fd13
Mikhail Kashkin <m@xen.ru>
2018-09-03 18:21:26 +0700

    Небольшие исправления к коду урока

--- demo/app.py
+++ demo/app.py
@@ -2,7 +2,6 @@ from aiohttp import web
 import jinja2
 import aiohttp_jinja2
 import asyncpgsa
-
 from .routes import setup_routes
 
 
@@ -14,14 +13,15 @@ async def create_app(config: dict):
         loader=jinja2.PackageLoader('demo', 'templates')
     )
     setup_routes(app)
-    app.on_startup.append(on_start_app)
+    app.on_startup.append(on_start)
+    app.on_cleanup.append(on_shutdown)
     return app
 
 
-async def on_start_app(app):
+async def on_start(app):
     config = app['config']
     app['db'] = await asyncpgsa.create_pool(dsn=config['database_uri'])
 
 
-async def on_close_app(app):
+async def on_shutdown(app):
     await app['db'].close()
