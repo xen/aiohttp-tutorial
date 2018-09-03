4b842a082765322530b00dfa165bce86906a58e0
Mikhail Kashkin <m@xen.ru>
2018-09-03 18:02:22 +0700

    Изменения:
    - добавили зависимости необходимые для работы с базой данных
    - создали подключение к базе

--- demo/app.py
+++ demo/app.py
@@ -1,6 +1,7 @@
 from aiohttp import web
 import jinja2
 import aiohttp_jinja2
+import asyncpgsa
 
 from .routes import setup_routes
 
@@ -13,4 +14,14 @@ async def create_app(config: dict):
         loader=jinja2.PackageLoader('demo', 'templates')
     )
     setup_routes(app)
+    app.on_startup.append(on_start_app)
     return app
+
+
+async def on_start_app(app):
+    config = app['config']
+    app['db'] = await asyncpgsa.create_pool(dsn=config['database_uri'])
+
+
+async def on_close_app(app):
+    await app['db'].close()
